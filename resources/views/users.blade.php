4e97b78e51ac447aba1807e9b94c6b7f128ea92d
Awil Omar <98050472+javaman-22@users.noreply.github.com>
2022-09-29 13:21:52 +0200

    Update users.blade.php

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,6 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        @foreach($Users as $users)
+            @if(count($users) < 1)
+            
             {{ __('Users') }}
         </h2>
     </x-slot>
